c7e7c73c872789ca926e4af06f400bc23ccc9aa1
marius <marius.budrauskas@gmail.com>
2019-07-05 12:44:37 +0300

    Commit

---
+++ mvc5-unit-of-work-example-master/UnitOfWorkExample.Data/Helpers/UnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitOfWorkExample.Data.MappingOverrides;
+using UnitOfWorkExample.Domain.Entities;
+using UnitOfWorkExample.Domain.Helpers;
+
+namespace UnitOfWorkExample.Data.Helpers
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private static readonly ISessionFactory _sessionFactory;
+        private ITransaction _transaction;
+
+        public ISession Session { get; set; }
+
+        static UnitOfWork() 
+        {
+            _sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008.ConnectionString(x => x.FromConnectionStringWithKey("UnitOfWorkExample")))
+                .Mappings(x => x.AutoMappings.Add(
+                    AutoMap.AssemblyOf<Product>(new AutomappingConfiguration()).UseOverridesFromAssemblyOf<ProductOverrides>()))
+                .ExposeConfiguration(config => new SchemaUpdate(config).Execute(false, true))
+                .BuildSessionFactory();
+        }
+
+        public UnitOfWork()
+        {
+            Session = _sessionFactory.OpenSession();
+        }
+
+        public void BeginTransaction()
+        {
+            _transaction = Session.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            try
+            {
+                if (_transaction != null && _transaction.IsActive)
+                    _transaction.Commit();
+            }
+            catch 
+            {
+                if (_transaction != null && _transaction.IsActive)
+                    _transaction.Rollback();
+
+                throw;
+            }
+            finally
+            {
+                Session.Dispose();
+            }
+        }
+
+        public void Rollback()
+        {
+            try
+            {
+                if (_transaction != null && _transaction.IsActive)
+                    _transaction.Rollback();
+            }
+            finally
+            {
+                Session.Dispose();
+            }
+        }
+    }
+}
